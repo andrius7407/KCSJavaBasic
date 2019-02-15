df2a1da988e97a7ebb1a7626ac6a2b528f40d709
Andrius Elsbergas <andrius.el@gmail.com>
2019-02-15 05:56:21 +0200

    Create Task15.java

---
+++ Task15.java
@@ -0,0 +1,42 @@
+/* 
+Parašyti metodą, kuris per parametrus pasiimtų tekstą ir per grąžinamą reikšmę praneštų kiek jame yra atskirų žodžių.
+*/
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Penkiolika {
+    public static int kiekTeksteZodziu(String fileName)throws IOException{
+        int kiekZodziu = 0;
+        BufferedReader bufferedReader = null;
+        try {
+            File file = new File(fileName);
+            bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                if(!line.trim().isEmpty()){
+                String []eilute = line.split(" ");
+                    kiekZodziu = kiekZodziu + eilute.length;
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                bufferedReader.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return kiekZodziu;
+    }
+    public static void main(String[] args) throws IOException {
+        int kiekZodziu = kiekTeksteZodziu("src\\tekstas.txt");
+        System.out.println(kiekZodziu);
+    }
+}
