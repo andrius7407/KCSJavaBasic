b3441ac1d5278db6f3b9f7b438c4cf19c87f6116
Andrius Elsbergas <andrius.el@gmail.com>
2019-02-17 08:24:30 +0200

    Create Task18Main.java

---
+++ Task18Main.java
@@ -0,0 +1,12 @@
+/*
+Faile surašyti žmonių vardai.
+Sukurti klasę kuri per konstruktorių paimtų failo vardą ir turėtų du metodus,
+vieną duomenų nuskaitymui iš failo į sąrašą, o kitą atvaizdavimui iš sąrašo į ekraną.
+*/
+
+public class Astuoniolika {
+
+    public static void main(String[] args) {
+        new Vardai("src\\vardai.txt");
+    }
+}
