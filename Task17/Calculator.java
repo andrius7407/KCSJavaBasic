85d42bbd7195c421161f5b026db00f8cc643d282
Andrius Elsbergas <andrius.el@gmail.com>
2019-03-17 14:15:05 +0200

    Create Calculator.java

---
+++ Task17/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    public double suma(double vienas, double du){
+        return vienas + du;
+    }
+    public double skirtumas(double vienas, double du){
+        return vienas - du;
+    }
+    public double sandauga(double vienas, double du){
+        return vienas * du;
+    }
+}
